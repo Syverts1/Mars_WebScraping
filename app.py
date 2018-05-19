5e1b920cc27583fd359fcccab8d3ce0c1f2ac9eb
laptopuser <ces271@rutgers.edu>
2018-05-19 17:40:55 -0400

    finished up

--- app.py
+++ app.py
@@ -9,8 +9,19 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
 
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape()
+    mars.update(
+        {},
+        mars_data,
+        upsert=True
+    )
+    return redirect("http://localhost:5000/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
